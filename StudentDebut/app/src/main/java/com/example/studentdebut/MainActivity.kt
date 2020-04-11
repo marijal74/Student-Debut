587320b448443739fb8e3fd3d0a6dd3d555484f9
marijal74 <mimosapudicapoison@gmail.com>
2020-04-11 20:33:54 +0200

    novi activity je dodat

---
+++ StudentDebut/app/src/main/java/com/example/studentdebut/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.studentdebut
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log.d
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        AddFilters.setOnClickListener(){
+            d("AddFilters Button", "Clicked. New Activity started")
+            startActivity(Intent(this, ListOfJobs::class.java))
+        }
+    }
+
+
+}

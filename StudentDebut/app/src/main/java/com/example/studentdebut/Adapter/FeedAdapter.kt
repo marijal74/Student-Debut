07ad4e203381ecd2fd064b696bc76a409154543e
milicar7 <radojicicmilica98@gmail.com>
2020-04-11 22:32:35 +0200

    Dodat Adapter i Listener

---
+++ StudentDebut/app/src/main/java/com/example/studentdebut/Adapter/FeedAdapter.kt
@@ -0,0 +1,81 @@
+package com.example.studentdebut.Adapter
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.studentdebut.Interface.ItemClickListener
+import com.example.studentdebut.Model.RSSObject
+import com.example.studentdebut.R
+
+class FeedViewHolder(itemView: View): RecyclerView.ViewHolder(itemView), View.OnClickListener, View.OnLongClickListener{
+
+    var txtTitle: TextView
+    var txtPubdate: TextView
+    var txtContent: TextView
+
+    private var itemClickListener : ItemClickListener?=null
+
+    init{
+
+        txtTitle = itemView.findViewById(R.id.txtTitle) as TextView
+        txtPubdate = itemView.findViewById(R.id.txtPubDate) as TextView
+        txtContent = itemView.findViewById(R.id.txtContent) as TextView
+
+         itemView.setOnClickListener(this)
+         itemView.setOnLongClickListener(this)
+    }
+
+    fun setItemClickListener(itemClickListener: ItemClickListener){
+
+        this.itemClickListener = itemClickListener
+    }
+    override fun onClick(v: View?){
+        itemClickListener!!.onClick(v, absoluteAdapterPosition, false)
+    }
+
+    override fun onLongClick(v: View?): Boolean {
+        itemClickListener!!.onClick(v, absoluteAdapterPosition, true)
+        return true
+    }
+
+}
+
+class FeedAdapter(private val rssObject: RSSObject, private val mContext: Context): RecyclerView.Adapter<FeedViewHolder>(){
+
+    private  val inflater: LayoutInflater
+
+    init{
+
+        inflater = LayoutInflater.from(mContext)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeedViewHolder {
+        val itemView = inflater.inflate(R.layout.row, parent, false)
+        return FeedViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return rssObject.items.size
+    }
+
+    override fun onBindViewHolder(holder: FeedViewHolder, position: Int) {
+        holder.txtTitle.text = rssObject.items[position].title
+        holder.txtContent.text = rssObject.items[position].content
+        holder.txtPubdate.text = rssObject.items[position].pubDate
+
+        holder.setItemClickListener(ItemClickListener{ view, position, isLongClick ->
+
+            if(!isLongClick){
+
+                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(rssObject.items[position].link))
+                mContext.startActivity(browserIntent)
+            }
+        })
+    }
+
+
+}
--- StudentDebut/app/src/main/java/com/example/studentdebut/Model/Item.kt
+++ StudentDebut/app/src/main/java/com/example/studentdebut/Model/Item.kt
@@ -1,6 +1,6 @@
 package com.example.studentdebut.Model
 
-data class Item (val title:String, val pubDate:String, val link:String, val guid:String,
-                    val enclosure:Object, val categories:List<String>){
+data class Item (val title:String, val pubDate:String, val link:String, val guid:String, val author: String,
+                 val thumbnail: String, val description: String, val content: String, val enclosure:Object, val categories:List<String>){
 
 }
